2c9081e2edc0760bc95ac43fb87f4ab7af1daa7e
Ross Morsali <ross@weareelevate.com>
2021-08-19 11:07:31 +0200

    Fix formatting in autoload.php

--- autoload.php
+++ autoload.php
@@ -5,33 +5,33 @@ function speed_up_autoloader( $class_name ) {
 
 	// These should be changed for your particular plugin requirements
 	$parent_namespace = 'Speed_Up';
-    $classes_subfolder = 'includes';
-	
-    if ( false !== strpos( $class_name, $parent_namespace ) ) {
-        $classes_dir = realpath( plugin_dir_path( __FILE__ ) ) . DIRECTORY_SEPARATOR . $classes_subfolder . DIRECTORY_SEPARATOR;
+	$classes_subfolder = 'includes';
 
-        // Project namespace
-        $project_namespace = $parent_namespace . '\\';
-        $length = strlen( $project_namespace );
+	if ( false !== strpos( $class_name, $parent_namespace ) ) {
+		$classes_dir = realpath( plugin_dir_path( __FILE__ ) ) . DIRECTORY_SEPARATOR . $classes_subfolder . DIRECTORY_SEPARATOR;
 
-        // Remove top level namespace (that is the current dir)
-        $class_file = substr( $class_name, $length );
-        // Swap underscores for dashes and lowercase
-        $class_file = str_replace( '_', '-', strtolower( $class_file ) );
+		// Project namespace
+		$project_namespace = $parent_namespace . '\\';
+		$length = strlen( $project_namespace );
 
-        // Prepend `class-` to the filename (last class part)
-        $class_parts = explode( '\\', $class_file );
-        $last_index = count( $class_parts ) - 1;
-        $class_parts[ $last_index ] = 'class-' . $class_parts[ $last_index ];
+		// Remove top level namespace (that is the current dir)
+		$class_file = substr( $class_name, $length );
+		// Swap underscores for dashes and lowercase
+		$class_file = str_replace( '_', '-', strtolower( $class_file ) );
 
-        // Join everything back together and add the file extension
-        $class_file = implode( DIRECTORY_SEPARATOR, $class_parts ) . '.php';
-        $location = $classes_dir . $class_file;
+		// Prepend `class-` to the filename (last class part)
+		$class_parts = explode( '\\', $class_file );
+		$last_index = count( $class_parts ) - 1;
+		$class_parts[ $last_index ] = 'class-' . $class_parts[ $last_index ];
 
-        if ( ! is_file( $location ) ) {
-            return;
-        }
+		// Join everything back together and add the file extension
+		$class_file = implode( DIRECTORY_SEPARATOR, $class_parts ) . '.php';
+		$location = $classes_dir . $class_file;
 
-        require_once $location;
-    }
+		if ( ! is_file( $location ) ) {
+			return;
+		}
+
+		require_once $location;
+	}
 }
